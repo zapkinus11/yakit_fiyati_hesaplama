8b0adf654e243b7403e48c2d38a93cfa2d9329f2
Ahmet <110814262+zapkinus11@users.noreply.github.com>
2024-03-21 14:45:00 +0300

    yakit-fiyati-hesaplama-Csharp
    aracınızın yakıtını 100km üzerinden yaptığınız ortalama tüketime ve girdiğiniz yakıt fiyatına göre hesaplamanız için kodlanmış basit c# uygulaması

---
+++ yakit/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace yakit
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void HesaplaButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double km = double.Parse(kilometreTextBox.Text);
+                double yakitTuketimi = double.Parse(yakitTuketimiTextBox.Text);
+                double yakitFiyati = double.Parse(yakitFiyatiTextBox.Text);
+
+                // yakıt hesaplama
+                double yakitMaliyeti = (km / 100) * yakitTuketimi * yakitFiyati;
+
+                sonucLabel.Content = "Toplam Kullanılan Yakıt Maliyeti: " + yakitMaliyeti.ToString("C");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen sayısal değer giriniz!!!", "Hata", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
